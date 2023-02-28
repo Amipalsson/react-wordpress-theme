c8126c68b56b1650945de6a2104d7a3eecfd97ca
digitalisland-anna <anna.paleologos@digitalisland.se>
2023-03-01 00:22:26 +0100

    Registered menu and added functions

--- wp-content/themes/react-theme/functions.php
+++ wp-content/themes/react-theme/functions.php
@@ -14,7 +14,55 @@ function ap_enqueue_scripts()
   $config = array(
     // TODO: Add any theme variables needed in react
     'rest_url' => rest_url(),
+    'menu' => ap_get_menu('main-menu'),
   );
   wp_localize_script('theme', 'wp_config', $config);
 }
 add_action('wp_enqueue_scripts', 'ap_enqueue_scripts');
+
+register_nav_menus(array(
+  'main-menu' => 'Main Menu'
+));
+
+/**
+ * Get WordPress menu by menu location
+ * 
+ * @return array
+ */
+function ap_get_menu($location)
+{
+    $output = [];
+    $locations = get_nav_menu_locations();
+    $menu = wp_get_nav_menu_object($locations[$location]);
+    $menu_items = wp_get_nav_menu_items($menu);
+
+    foreach ($menu_items as $item) {
+
+        $data = [
+            'ID' => $item->ID,
+            'title' => $item->title,
+            'url' => '/' . ap_strip_site_url($item->url),
+            'parent' => intval($item->menu_item_parent),
+        ];
+        $output[] = $data;
+    }
+
+    return $output;
+}
+
+/**
+ * Remove WordPress site url and trailing slash from url
+ *
+ * @param $url
+ * @return string
+ */
+function ap_strip_site_url($url)
+{
+    $base_url = site_url();
+    $url = trailingslashit($url);
+    $url = substr($url, strlen($base_url) + 1);
+    if (substr($url, -1) == '/') {
+        $url = substr($url, 0, -1);
+    }
+    return $url;
+}
