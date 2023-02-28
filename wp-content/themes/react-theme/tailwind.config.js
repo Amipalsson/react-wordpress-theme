58bb1f1a5cab034f4888f1dc832597c236c52ef8
digitalisland-anna <anna.paleologos@digitalisland.se>
2023-03-01 00:23:17 +0100

    Added tailwind css

--- wp-content/themes/react-theme/tailwind.config.js
+++ wp-content/themes/react-theme/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:[
-    './src/**/*.{js,jsx,ts,tsx}',
-    "./*.php",
-		"./src/css/*.css",
-		"./src/js/*.js",
-		"./functions/*.php",
-  ],
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+    ],
   theme: {
     extend: {},
   },
